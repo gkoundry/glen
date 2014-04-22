ff3fcf7cf583fbe471ae04be1d6f4ed3a5da3e1e
Glen Koundry <glen@datarobot.com>
2014-04-22 14:34:39 -0400

    werwer

--- pp/rgbm7r.py
+++ pp/rgbm7r.py
@@ -17,7 +17,7 @@ LR=0.03
 if len(sys.argv)>1:
     COL1=sys.argv[1]
 else:
-    COL1='D'
+    COL1='G'
 LEVELS={
     'A': ('0','1','2'),
     'B': ('0','1'),
@@ -38,13 +38,13 @@ ans=X.pop('ans')
 
 fo=open('pred'+COL1+'.csv','w')
 imp = Imputer(strategy='most_frequent')
-for mf in (7,):
-#for mf in (5,):
+#for mf in (3,7,):
+for mf in (5,):
     #for mn in (1,20,100,):
-    for mn in (1,):
+    for mn in (5,):
         ttr = 0
         #for tr in (100,200,400,):
-        for tr in (200,):
+        for tr in (100,):
             scp = 0
             scl = 0
             rsp = 0
@@ -63,27 +63,24 @@ for mf in (7,):
                 lasttest = last.values[test]
                 anstest = ans.values[test]
                 idtest = rid.values[test].astype(float)
-                m=rgbm.gbm_fit(xtrain,ytrain,nTrain=xtrain.shape[0],bag_fraction=1,n_trees=tr,verbose=False,keep_data=False,n_minobsinnode=mn,distribution='multinomial',interaction_depth=mf,shrinkage=LR) #, w=wtrain[rows]*2.0)
+                m=rgbm.gbm_fit(xtrain,ytrain,nTrain=xtrain.shape[0],bag_fraction=1,n_trees=tr,verbose=False,keep_data=False,n_minobsinnode=mn,distribution='gaussian',interaction_depth=mf,shrinkage=LR) #, w=wtrain[rows]*2.0)
                 ppp=rgbm.predict_gbm(m,xtest,n_trees=tr,type="response")
-                pp = np.array(ppp)
-                levels = np.array(ppp.names[1]).astype(int)
-                #print pp.shape
-                pred=np.argmax(pp,axis=1)
+                pred = np.around(np.minimum(4,np.maximum(1,np.array(ppp))))
                 for i,a in enumerate(ytest):
                     #print '%s %07d %07d %d %s %s' % (idtest[i],anstest[i],lasttest[i],int(levels[pred[i]]),int(levels[pred[i]])==int(a) and rtest[i]==1,lstest[i]==int(a) and rtest[i]==1)
                     if int(lstest[i])==int(a):
                         scl += 1
                         if rtest[i]==1:
                             rsl += 1
-                    fo.write("%07d,%02d\n" % (idtest[i],levels[pred[i]]))
+                    fo.write("%07d,%02d\n" % (idtest[i],pred[i]))
                     aa = '%07d' % anstest[i]
                     ab = '%07d' % lasttest[i]
-                    p1 = '%02d' % int(levels[pred[i]])
+                    p1 = '%02d' % int(pred[i])
                     ab=list(ab)
                     ab[ord(COL1)-65] = p1[0]
                     ab=''.join(ab)
                     #print '%s %s %s %d' % ( idtest[i],aa,ab, int(int(levels[pred[i]])==int(a) and rtest[i]==1))
-                    if int(levels[pred[i]])==int(a):
+                    if int(pred[i])==int(a):
                         scp += 1
                         if rtest[i]==1:
                             rsp += 1
