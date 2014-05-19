292870d4ee7909d4c15ea958237043aec8f3b5cb
gkoundry <glen@datarobot.com>
2014-05-19 14:05:21 -0400

    werwe

--- pp/flatten12t.py
+++ pp/flatten12t.py
@@ -40,7 +40,6 @@ levels = { 'A':set(),'B':set(),'C':set(),'D':set(),'E':set(),'F':set(),'G':set()
 
 last = {}
 llast = {}
-ans = {}
 ca = {}
 costlvl = {}
 costdir = {}
@@ -69,6 +68,7 @@ cost2={}
 wavg={}
 wtot={}
 uniq={}
+lastid=None
 states = set()
 for col in ('A','B','C','D','E','F','G'):
     prior[col]=defaultdict(int)
@@ -79,84 +79,82 @@ hour={}
 time1={}
 time2={}
 quotes = defaultdict(int)
-f=open('trains1.csv','r')
-#f=open('train.csv','r')
+#f=open('trains1.csv','r')
+f=open('test_v2.csv','r')
 h=f.readline()
 for l in f:
     a=l.rstrip().split(',')
     id=a[0]
-    rt=a[2]
 
     cs=float(a[24])
-    if rt=='1':
-        ans[id] = a[17:24]
-        for col in ('A','B','C','D','E','F','G'):
-            val = a[ord(col)-48]
-            levels[col].add(val)
+    if lastid and id!=lastid:
         tcost = 0
         ccost = 0
+    for col in ('A','B','C','D','E','F','G'):
+        val = a[ord(col)-48]
+        levels[col].add(val)
+    gs[id]=a[7]
+    ho[id]=a[8]
+    ca[id]=a[9]
+    dp[id]=a[16]
+    mc[id]=a[14]
+    ao[id]=a[12]
+    cv[id],cvm[id] = carval(a[10])
+    rf[id],rfm[id] = rfx(a[11])
+    cp[id],cpm[id] = rfx(a[15])
+    dp[id],dpm[id] = rfx(a[16])
+    day[id]=a[3]
+    hour[id]=a[4].split(':')[0]
+    if id not in time1:
+        time1[id] = cvt(a[4])
+    time2[id] = cvt(a[4])
+    if id not in cost1:
+        cost1[id] = cs
+        cost2[id] = cs
     else:
-        gs[id]=a[7]
-        ho[id]=a[8]
-        ca[id]=a[9]
-        dp[id]=a[16]
-        mc[id]=a[14]
-        ao[id]=a[12]
-        cv[id],cvm[id] = carval(a[10])
-        rf[id],rfm[id] = rfx(a[11])
-        cp[id],cpm[id] = rfx(a[15])
-        dp[id],dpm[id] = rfx(a[16])
-        day[id]=a[3]
-        hour[id]=a[4].split(':')[0]
-        if id not in time1:
-            time1[id] = cvt(a[4])
-        time2[id] = cvt(a[4])
-        if id not in cost1:
-            cost1[id] = cs
-            cost2[id] = cs
-        else:
-            if cs>cost2[id]:
-                cost2[id]=cs
-            if cs<cost1[id]:
-                cost1[id]=cs
-        states.add(a[5])
-        state[id]=a[5]
-        ad[id]=int(a[12])-int(a[13])
-        quotes[id] += 1
-        if tcost > 0:
-            costdir[id] = cs / (tcost/ccost)
-        tcost += cs
-        ccost += 1
-        costlvl[id] = tcost / ccost
-        if id not in freq:
-            freq[id] = {}
-            lcost[id] = {}
-            hist[id] = {}
-            uniq[id] = {}
-            wavg[id]=defaultdict(float)
-            wtot[id]=defaultdict(int)
-            for col in ('A','B','C','D','E','F','G'):
-                uniq[id][col] = set()
-                freq[id][col] = defaultdict(int)
-                lcost[id][col]=defaultdict(int)
-                hist[id][col]=defaultdict(int)
+        if cs>cost2[id]:
+            cost2[id]=cs
+        if cs<cost1[id]:
+            cost1[id]=cs
+    states.add(a[5])
+    state[id]=a[5]
+    ad[id]=int(a[12])-int(a[13])
+    quotes[id] += 1
+    if tcost > 0:
+        costdir[id] = cs / (tcost/ccost)
+    tcost += cs
+    ccost += 1
+    costlvl[id] = tcost / ccost
+    if id not in freq:
+        freq[id] = {}
+        lcost[id] = {}
+        hist[id] = {}
+        uniq[id] = {}
+        wavg[id]=defaultdict(float)
+        wtot[id]=defaultdict(int)
         for col in ('A','B','C','D','E','F','G'):
-            wavg[id][col]+=int(a[ord(col)-48])*(2**int(a[1]))
-            wtot[id][col]+=(2**int(a[1]))
-            uniq[id][col].add(a[ord(col)-48])
-            freq[id][col][a[ord(col)-48]] += 1
-            lcost[id][col][a[ord(col)-48]] += cs
-            prior[col][a[ord(col)-48]] += 1
-            if id in last and a[ord(col)-48] == last[id][ord(col)-65]:
-                hist[id][col] += 1
-            else:
-                hist[id][col]=1
-        if id in last:
-            llast[id] = last[id][:]
-        last[id] = a[17:24]
+            uniq[id][col] = set()
+            freq[id][col] = defaultdict(int)
+            lcost[id][col]=defaultdict(int)
+            hist[id][col]=defaultdict(int)
+    for col in ('A','B','C','D','E','F','G'):
+        wavg[id][col]+=int(a[ord(col)-48])*(2**int(a[1]))
+        wtot[id][col]+=(2**int(a[1]))
+        uniq[id][col].add(a[ord(col)-48])
+        freq[id][col][a[ord(col)-48]] += 1
+        lcost[id][col][a[ord(col)-48]] += cs
+        prior[col][a[ord(col)-48]] += 1
+        if id in last and a[ord(col)-48] == last[id][ord(col)-65]:
+            hist[id][col] += 1
+        else:
+            hist[id][col]=1
+    if id in last:
+        llast[id] = last[id][:]
+    last[id] = a[17:24]
+    lastid=id
 
-f=open('train4_12'+target+'.csv','w')
-f.write('id,y,ls,ans,last,rest')
+f=open('trainf12t'+target+'.csv','w')
+f.write('id,ls,last')
 for col in ('A','B','C','D','E','F','G'):
     for lvl in sorted(list(levels[col])):
         f.write(',last'+col+lvl)
@@ -199,12 +197,8 @@ for col in ('A','B','C','D','E','F','G'):
 #f.write(',csrt1')
 #f.write(',csrt2')
 f.write('\n')
-for id in ans.keys():
-    rest = 1
-    for col in ('A','B','C','D','E','F','G'):
-        if col!=target and ans[id][tval(col)]!=last[id][tval(col)]:
-            rest = 0
-    f.write('%s,%s,%s,%s,%s,%d' % (id,ans[id][tval(target)],last[id][tval(target)],''.join(ans[id]),''.join(last[id]),rest))
+for id in ca.keys():
+    f.write('%s,%s,%s' % (id,last[id][tval(target)],''.join(last[id])))
     for col in ('A','B','C','D','E','F','G'):
         for lvl in sorted(list(levels[col])):
             f.write(',%d' % int(last[id][tval(col)]==lvl))
