f1bf76a2a247e19803ba4b8b3a55eea33b76708e
gkoundry <glen@datarobot.com>
2014-11-18 09:28:41 -0500

    Add bernoulli support

--- gam/gam.c
+++ gam/gam.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 #include "gam.h"
 
 #define WINDOW 100
@@ -74,24 +75,35 @@ double loss, pred;
 	return sqrt(loss/rows);
 }
 
-void smooth_column(double *rsd, double *smoothX, int *sortedX, int rows, int cols, int c) {
+void smooth_column(double *rsd, double *smoothX, int *sortedX, double *w, int rows, int cols, int c) {
 	
 int r;
-double wtot;
+double wtot,wsum,mean;
 	
+	mean = 0;
 	wtot = 0;
+	wsum = 0;
 	for(r=0; r<WINDOW; r++) {
-		wtot += rsd[sortedX[c*rows+r]];
+		wtot += rsd[sortedX[c*rows+r]] * w[sortedX[c*rows+r]];
+		wsum += w[sortedX[c*rows+r]];
 	}
 	for(r=0; r<WINDOW/2; r++) {
-		smoothX[r*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) / (WINDOW - 1);
+		smoothX[sortedX[c*rows+r]*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) / (wsum - w[sortedX[c*rows+r]]);
+		mean += smoothX[sortedX[c*rows+r]*cols+c];
 	}
 	for(r=WINDOW/2; r<rows-WINDOW/2; r++) {
-		smoothX[r*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) /  WINDOW;
+		smoothX[sortedX[c*rows+r]*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) /  (wsum - w[sortedX[c*rows+r]]);
+		mean += smoothX[sortedX[c*rows+r]*cols+c];
 		wtot = wtot - rsd[sortedX[c*rows+(r-WINDOW/2)]] + rsd[sortedX[c*rows+(r+WINDOW/2)]];
+		wsum = wsum - w[sortedX[c*rows+(r-WINDOW/2)]] + w[sortedX[c*rows+(r+WINDOW/2)]];
 	}
 	for(r=rows-WINDOW/2; r<rows; r++) {
-		smoothX[r*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) / (WINDOW - 1);
+		smoothX[sortedX[c*rows+r]*cols+c] = (wtot - rsd[sortedX[c*rows+r]]) / (wsum - w[sortedX[c*rows+r]]);
+		mean += smoothX[sortedX[c*rows+r]*cols+c];
+	}
+	mean /= rows;
+	for(r=0; r<rows; r++) {
+		smoothX[sortedX[c*rows+r]*cols+c] -= mean;
 	}
 }
 
@@ -114,11 +126,83 @@ struct sort_pair *sort_pairs;
 	free(sort_pairs);
 }
 
+double inverse_link(double eta) {
+
+double e;
+
+	e = 1/(1+exp(-eta));
+	if(e<0.000001) e=0.000001;
+	if(e>0.999999) e=0.999999;
+	return e;
+}
+
+#define MAX_LOG_PRED 15
+double inv_link_function_d(double eta) {
+
+double e;
+
+	if(eta>MAX_LOG_PRED) eta = MAX_LOG_PRED;
+	e=exp(eta);
+	e = e/((1+e)*(1+e));
+	if(e<DBL_EPSILON)
+		return DBL_EPSILON;
+	else
+		return e;
+}
+
+void get_z(double *z, double *y, double *smoothX, int rows, int cols, double alpha) {
+
+int r,c;
+double lde,eta;
+
+	for(r=0; r<rows; r++) {
+		eta = alpha;
+		for(c=0; c<cols; c++) {
+			eta += smoothX[r*cols+c];
+		}
+		lde = inv_link_function_d(eta);
+		z[r] = eta + (y[r] - inverse_link(eta)) / lde;
+	}
+}
+
+double variance(double mu) {
+	if(mu<0.000001) mu=0.000001;
+	else if(mu>0.999999) mu=0.999999;
+	return mu*(1-mu);
+}
+
+void get_weights(double *w, double *smoothX, int rows, int cols, double alpha) {
+
+double eta,lde,mu;
+int r,c;
+
+	for(r=0; r<rows; r++) {
+		eta = alpha;
+		for(c=0; c<cols; c++) {
+			eta += smoothX[r*cols+c];
+		}
+		lde = inv_link_function_d(eta);
+		mu = inverse_link(eta);
+		w[r] = 1/sqrt(lde*lde/variance(mu)); 
+		//w[r] = 1/(lde*lde/variance(mu)); 
+	}
+}
+
+void weight_smoothx(double *wsx, double *w, double *smoothX, int rows, int cols, int c) {
+
+int r;
+
+	for(r=0;r<rows;r++) {
+		wsx[r] = w[r] * smoothX[r*cols+c];
+	}
+}
+
 GAM *fit(double *X, double *y, int rows, int cols, double tol) {
 
-double *rsd, loss, old_loss;
+double *rsd, loss, old_loss, *z, *w, *wsx;
 int c;
 GAM *model;
+int t;
 
 	fprintf(stderr,"Start fit.\n");
 	model = (GAM *)malloc(sizeof(GAM));
@@ -128,23 +212,41 @@ GAM *model;
 	rsd = (double *)malloc(rows * sizeof(double));
 	model->smoothX = (double *)malloc(rows * cols * sizeof(double));
 	model->sortedX = (int *)malloc(rows * cols * sizeof(int));
+	z = (double *)malloc(rows * sizeof(double));
+	w = (double *)malloc(rows * sizeof(double));
+	wsx = (double *)malloc(rows * sizeof(double));
 	old_loss = 1e20;
 	loss = 0;
 	model->alpha = calculate_alpha(y, rows);
+
+
+	model->alpha = log(model->alpha) - log(1-model->alpha);
+
+
 	initialize_smoothX(model->smoothX, rows, cols);
 	fprintf(stderr,"Start sort.\n");
 	get_sorted_indices(X, model->sortedX, rows, cols);
 	fprintf(stderr,"Start backfit.\n");
 	while(fabs(old_loss-loss)/old_loss > tol) {
+		get_z(z, y, model->smoothX, rows, cols, model->alpha);
+		get_weights(w, model->smoothX, rows, cols, model->alpha);
 		for(c=0; c<cols; c++) {
-			get_partial_residuals(rsd, model->smoothX, y, rows, cols, model->alpha, c);
-			smooth_column(rsd, model->smoothX, model->sortedX, rows, cols, c);
+			get_partial_residuals(rsd, model->smoothX, z, rows, cols, model->alpha, c);
+			smooth_column(rsd, model->smoothX, model->sortedX, w, rows, cols, c);
+			//for(t=0;t<1000;t++) printf("%f %f\n",model->smoothX[t*cols+c],X[t*cols+c]);
+			//exit(0);
 		}
 		old_loss = loss;
 		loss = calculate_loss(y, model->smoothX, rows, cols, model->alpha);
 		fprintf(stderr,"Loss %f -> %f\n", old_loss, loss);
 	}
+	fprintf(stderr,"Coverged\n");
+	//for(t=0;t<1000;t++) printf("%f %f %f\n",y[t],z[t],w[t]);
+	//exit(0);
 	free(rsd);
+	free(z);
+	free(w);
+	free(wsx);
 	fprintf(stderr,"End fit.\n");
 	return model;
 }
@@ -172,7 +274,7 @@ int first,last,middle;
 	while(first <= last) {
 		if(model->X[model->sortedX[c*model->rows+middle]*model->cols+c] < x) {
 			first = middle + 1;    
-		} else if(model->X[model->sortedX[c*model->rows+middle]*model->cols+c] < x) {
+		} else if(model->X[model->sortedX[c*model->rows+middle]*model->cols+c] == x) {
 			return model->smoothX[model->sortedX[c*model->rows+middle]*model->cols+c];
 		} else {
 			last = middle - 1;
--- gam/test.c
+++ gam/test.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include "gam.h"
 
 #define ROWS 1000
+#define ROWSP 15000
 #define COLS 10
 
 double X[ROWS][COLS], y[ROWS], pred[ROWS];
+double Xv[ROWSP][COLS], yv[ROWSP], predv[ROWSP];
 char line[4096];
 
 int main() {
@@ -14,27 +17,51 @@ int main() {
 FILE *fp;
 int r,c;
 char *p;
+double sc,scm,tmean;
 GAM *model;
 
 	fp=fopen("credit-train-1000.csv", "r");
 	p = fgets(line,4096,fp);
 	r = 0;
+	tmean = 0;
 	while(fgets(line,4096,fp)) {
 		p = strtok(line, ",");
 		p = strtok(NULL, ",");
 		y[r] = atof(p);
+		tmean += y[r];
 		for(c=0;c<COLS;c++) {
 			p = strtok(NULL, ",");
 			X[r][c] = atof(p);
 		}
 		r++;
 	}
+	tmean /= ROWS;
+	fclose(fp);
+	fp=fopen("credit-train-small.csv", "r");
+	p = fgets(line,4096,fp);
+	r = 0;
+	while(fgets(line,4096,fp)) {
+		p = strtok(line, ",");
+		p = strtok(NULL, ",");
+		yv[r] = atof(p);
+		for(c=0;c<COLS;c++) {
+			p = strtok(NULL, ",");
+			Xv[r][c] = atof(p);
+		}
+		r++;
+	}
 	fclose(fp);
 	model = fit((double *)X, y, ROWS, COLS, 0.000000001);
-	predict((double *)X, pred, ROWS, model);
-	for(r=0;r<1000;r++) {
-		printf("%f %f\n",pred[r],y[r]);
+	predict((double *)Xv, predv, ROWSP, model);
+	sc = 0;
+	scm = 0;
+	for(r=0;r<ROWSP;r++) {
+		predv[r] = 1/(1+exp(-predv[r]));
+		printf("%f %f\n",predv[r],yv[r]);
+		sc -= yv[r] * log(predv[r]) + (1-yv[r])*log(1-predv[r]);
+		scm -= yv[r] * log(tmean) + (1-yv[r])*log(1-tmean);
 	}
+	printf("%f %f\n",sc/ROWSP,scm/ROWSP);
 	free_GAM(model);
 
 	return 0;
